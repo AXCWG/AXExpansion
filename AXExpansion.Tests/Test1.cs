d62d0953e574ab7164e469884e407f5fca8ae098
AXCWG <xiey0@qq.com>
2026-03-12 20:57:04 +0800

    See https://github.com/AXCWG/SimpleJsonStorage

--- AXExpansion.Tests/Test1.cs
+++ AXExpansion.Tests/Test1.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using AXExpansion.AXHelper.Helpers;
-using SimpleJsonStorage;
 
 namespace AXExpansion.Tests;
 
@@ -16,9 +15,10 @@ public sealed class Test1
             Content = "WWWWWWW"
         };
         Console.WriteLine(
-        h.Serialize(HiSerializerContext.Default.Hi));
-        
+            h.Serialize(HiSerializerContext.Default.Hi));
+
     }
+
     [TestMethod]
     public void CbSerialize()
     {
@@ -26,20 +26,17 @@ public sealed class Test1
         {
             Content = "IPWJDIAI"
         };
-        var serializedProcessed = h.Serialize(c =>
-        {
-            return c.Replace(":", "");
-        });
+        var serializedProcessed = h.Serialize(c => { return c.Replace(":", ""); });
         (serializedProcessed).PrintLn();
     }
 
     [TestMethod]
     public void IntegratedObjectPrint()
     {
-        
-        new[] {1,2,34,54,5,6,6,6,6,66}.SPrint();
-        new List<int>{1,3,3,5,45,4,5,4,54,4}.SPrintLn();
-        Enumerable.Range(2,566).SPrintLn();
+
+        new[] { 1, 2, 34, 54, 5, 6, 6, 6, 6, 66 }.SPrint();
+        new List<int> { 1, 3, 3, 5, 45, 4, 5, 4, 54, 4 }.SPrintLn();
+        Enumerable.Range(2, 566).SPrintLn();
     }
 
     [TestMethod]
@@ -47,58 +44,58 @@ public sealed class Test1
     {
         "{0}, {1}".PrintLnF(2, 4);
     }
-
-    [TestMethod]
-    public void Storage()
-    {
-        var p = new ProgramStorage<Hi>("axexpansion.test", "test");
-        p.Set(new Hi
-        {
-            Content = "Hello World",
-            Fucked = true, _dddd = "22222222222"
-        });
-    }
-
-    [TestMethod]
-    public void EnumerableStorage()
-    {
-        var p = new ProgramStorageSet<string>("axexpansion.test", "ienumerableTest");
-        
-    }
-
-    [TestMethod]
-    public void StoragePoolTest()
-    {
-        var p = new SampleStoragePool("axexpansion.test");
-        p.Strings.Add("Hello world. ");
-    }
-
-    [TestMethod]
-    public void BasicInputOutputHandling()
-    {
-        var p = new AppStorage();
-        p.AlbumCovers.Add(new()
-        {
-            Uuid = Guid.NewGuid().ToString(),
-            Name = "Dark side of the moon"
-        });
-        p.Ids.Add(new()
-        {
-            Uuid = Guid.NewGuid().ToString(),
-            Host = "8.141.5.12"
-        });
-        p.Settings.Set(i=>i.IsOn, false);
-        p.Settings.Get(i => i.Name)?.Length.PrintLn();
-        p.Ids.SPrintLn();
-        var x = Enumerable.Range(1, 2555).Select(_ => new Id
-        {
-            Uuid = Guid.NewGuid().ToString(),
-            Host = "2222"
-        }).ToList();
-        p.Ids.AddRange(x); 
-    }
 }
-
+//     [TestMethod]
+//     public void Storage()
+//     {
+//         var p = new ProgramStorage<Hi>("axexpansion.test", "test");
+//         p.Set(new Hi
+//         {
+//             Content = "Hello World",
+//             Fucked = true, _dddd = "22222222222"
+//         });
+//     }
+//
+//     [TestMethod]
+//     public void EnumerableStorage()
+//     {
+//         var p = new ProgramStorageSet<string>("axexpansion.test", "ienumerableTest");
+//         
+//     }
+//
+//     [TestMethod]
+//     public void StoragePoolTest()
+//     {
+//         var p = new SampleStoragePool("axexpansion.test");
+//         p.Strings.Add("Hello world. ");
+//     }
+//
+//     [TestMethod]
+//     public void BasicInputOutputHandling()
+//     {
+//         var p = new AppStorage();
+//         p.AlbumCovers.Add(new()
+//         {
+//             Uuid = Guid.NewGuid().ToString(),
+//             Name = "Dark side of the moon"
+//         });
+//         p.Ids.Add(new()
+//         {
+//             Uuid = Guid.NewGuid().ToString(),
+//             Host = "8.141.5.12"
+//         });
+//         p.Settings.Set(i=>i.IsOn, false);
+//         p.Settings.Get(i => i.Name)?.Length.PrintLn();
+//         p.Ids.SPrintLn();
+//         var x = Enumerable.Range(1, 2555).Select(_ => new Id
+//         {
+//             Uuid = Guid.NewGuid().ToString(),
+//             Host = "2222"
+//         }).ToList();
+//         p.Ids.AddRange(x); 
+//     }
+// }
+//
 public class Hi
 {
     public string? Content { get; set; }
@@ -106,39 +103,39 @@ public class Hi
     public string _dddd;
 
 }
-
+//
 [JsonSerializable(typeof(Hi))]
 [JsonSerializable(typeof(string))]
 public partial class HiSerializerContext : JsonSerializerContext
 {
 }
-
-public class AlbumCover
-{
-    public required string Uuid { get; set; }
-    public required string Name { get; set; }
-}
-
-public class Id
-{
-    public required string Uuid { get; set; }
-    public required string Host { get; set; }
-}
-
-public class Setting
-{
-    public bool IsAdmin { get; set; }
-    public string? Name { get; set; }
-    public bool IsOn{ get; set; }
-}
-public sealed class AppStorage : StoragePool
-{
-    public ProgramStorageSet<AlbumCover> AlbumCovers { get; set; } = null!;
-    public ProgramStorageSet<Id> Ids { get; set; } = null!;
-    public ProgramStorage<Setting> Settings { get; set; } = null!; 
-    
-    public AppStorage()
-    {
-        OnConfiguring("com.axcwg.test", new JsonSerializerOptions{WriteIndented = true});
-    }
-}
+//
+// public class AlbumCover
+// {
+//     public required string Uuid { get; set; }
+//     public required string Name { get; set; }
+// }
+//
+// public class Id
+// {
+//     public required string Uuid { get; set; }
+//     public required string Host { get; set; }
+// }
+//
+// public class Setting
+// {
+//     public bool IsAdmin { get; set; }
+//     public string? Name { get; set; }
+//     public bool IsOn{ get; set; }
+// }
+// public sealed class AppStorage : StoragePool
+// {
+//     public ProgramStorageSet<AlbumCover> AlbumCovers { get; set; } = null!;
+//     public ProgramStorageSet<Id> Ids { get; set; } = null!;
+//     public ProgramStorage<Setting> Settings { get; set; } = null!; 
+//     
+//     public AppStorage()
+//     {
+//         OnConfiguring("com.axcwg.test", new JsonSerializerOptions{WriteIndented = true});
+//     }
+// }
